8d42e1e377207717b5cf0b26aa346e1f0c0b2458
gpolli <gpolli.dev@gmail.com>
2021-03-09 22:46:34 +0100

    Add card basic style

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
+/* Style */
+import './style.css';
 
-const CharacterCard = ({ character }) => {
+const CharacterCard = ({ character, parentClass }) => {
   const { name, status, species, type, gender, image } = character;
 
   return (
-    <div>
-      <img src={image} alt={name} />
-      <section>
-        <p>{name}</p>
-        <p>{status}</p>
-        <p>{species}</p>
-        <p>{type}</p>
-        <p>{gender}</p>
+    <div className={`${parentClass} character-card`}>
+      <img className="character-card__image" src={image} alt={name} />
+      <section className="character-card__registry">
+        <div className="character-card__info-group">
+          <p className="character-card__info-label">Name: </p>
+          <p className="character-card__info-value">{name}</p>
+        </div>
+        <div className="character-card__info-group">
+          <p className="character-card__info-label">Status: </p>
+          <p className="character-card__info-value">{status}</p>
+        </div>
+        <div className="character-card__info-group">
+          <p className="character-card__info-label">Species: </p>
+          <p className="character-card__info-value">{species}</p>
+        </div>
+        <div className="character-card__info-group">
+          <p className="character-card__info-label">Type: </p>
+          <p className="character-card__info-value">{type}</p>
+        </div>
+        <div className="character-card__info-group">
+          <p className="character-card__info-label">Gender: </p>
+          <p className="character-card__info-value">{gender}</p>
+        </div>
       </section>
     </div>
   );
--- src/components/CharactersList/index.js
+++ src/components/CharactersList/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 /* Components */
 import CharacterCard from '../CharacterCard';
+/* Style */
+import './style.css';
 
 const CharactersList = ({ characters }) => {
   return (
     <section>
       {
         characters.length
-          ? characters.map(character => (<CharacterCard character={character} />))
+          ? characters.map(character => (<CharacterCard character={character} parentClass="characters-list__character-item" />))
           : "No characters found"
       }
     </section>
