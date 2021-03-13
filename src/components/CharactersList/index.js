b8859e398e3aa1fe08918c882fbad659b6ee76d8
gpolli <gpolli.dev@gmail.com>
2021-03-13 22:41:20 +0100

    Fix void fields

--- src/components/CharactersList/index.js
+++ src/components/CharactersList/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 /* Components */
 import CharacterCard from '../CharacterCard';
 /* Helpers */
-import { objectIsEmpty } from '../../helpers/utils';
+import { capitalizeFirstLetter } from '../../helpers/utils';
 /* Style */
 import './style.css';
 
@@ -34,85 +34,51 @@ const CharactersList = ({ parentClass, content = { characters: [], location: [],
     const { name: originName } = origin;
     const { name: locationName } = location;
 
-    if (data.hasOwnProperty('locations') && data.hasOwnProperty('episodes')) {
-      // console.log('episodes: ', originName, locationName);
-
-      return {
-        static: {
-          image,
-          name,
-        },
-        dynamic: {
-          profile: {
-            status,
-            species,
-            type,
-            gender,
-          },
-          origin: {
-            name: originName !== "unknown" ? originName : "Unknown",
-            dimension: originName !== "unknown" ? data['locations'][originName]['dimension'] : "Unknown",
-            type: originName !== "unknown" ? data['locations'][originName]['type'] : "Unknown",
-            'amount of residents': originName !== "unknown" ? data['locations'][originName]['residents'].length : "Unknown",
-          },
-          location: {
-            name: locationName !== "unknown" ? locationName : "Unknown",
-            dimension: locationName !== "unknown" ? data['locations'][locationName]['dimension'] : "Unknown",
-            type: locationName !== "unknown" ? data['locations'][locationName]['type'] : "Unknown",
-            'amount of residents': locationName !== "unknown" ? data['locations'][locationName]['residents'].length : "Unknown",
-          },
-          episodes: getEpisodesInfo(getEpisodesIndexes(episodes), data['episodes']),
-        }
-      }
-    }
-
-    if (data.hasOwnProperty('locations')) {
-      // console.log('locations: ', originName, locationName);
-
-      return {
-        static: {
-          image,
-          name,
-        },
-        dynamic: {
-          profile: {
-            status,
-            species,
-            type,
-            gender,
-          },
-          origin: {
-            name: originName !== "unknown" ? originName : "Unknown",
-            dimension: originName !== "unknown" ? data['locations'][originName]['dimension'] : "Unknown",
-            type: originName !== "unknown" ? data['locations'][originName]['type'] : "Unknown",
-            'amount of residents': originName !== "unknown" ? data['locations'][originName]['residents'].length : "Unknown",
-          },
-          location: {
-            name: locationName !== "unknown" ? locationName : "Unknown",
-            dimension: locationName !== "unknown" ? data['locations'][locationName]['dimension'] : "Unknown",
-            type: locationName !== "unknown" ? data['locations'][locationName]['type'] : "Unknown",
-            'amount of residents': locationName !== "unknown" ? data['locations'][locationName]['residents'].length : "Unknown",
-          },
-          episodes: [],
-        }
-      }
-    }
-
-    return {
+    const presets = {
       static: {
         image,
         name,
       },
       dynamic: {
         profile: {
-          status,
-          species,
-          type,
-          gender,
+          status: status ? capitalizeFirstLetter(status) : "Unknown",
+          species: species ? capitalizeFirstLetter(species) : "Unknown",
+          type: type ? capitalizeFirstLetter(type) : "Unknown",
+          gender: gender ? capitalizeFirstLetter(gender) : "Unknown",
         },
         origin: {},
         location: {},
         episodes: [],
+      }
+    };
+
+    if (data.hasOwnProperty('locations')) {
+      presets.dynamic.origin = {
+        name: originName !== "unknown" ? capitalizeFirstLetter(originName) : "Unknown",
+        dimension: (data.locations?.[originName] && data.locations?.[originName]['dimension']) ? capitalizeFirstLetter(data['locations'][originName]['dimension']) : "Unknown",
+        type: (data.locations?.[originName] && data.locations?.[originName]['type']) ? capitalizeFirstLetter(data['locations'][originName]['type']) : "Unknown",
+        'amount of residents': (data.locations?.[originName] && data.locations?.[originName]['residents'] && Array.isArray(data.locations?.[originName]['residents'])) ? data['locations'][originName]['residents'].length : "Unknown",
+      };
+
+      presets.dynamic.location = {
+        name: locationName !== "unknown" ? capitalizeFirstLetter(locationName) : "Unknown",
+        dimension: (data.locations?.[locationName] && data.locations?.[locationName]['dimension']) ? capitalizeFirstLetter(data['locations'][locationName]['dimension']) : "Unknown",
+        type: (data.locations?.[locationName] && data.locations?.[locationName]['type']) ? capitalizeFirstLetter(data['locations'][locationName]['type']) : "Unknown",
+        'amount of residents': (data.locations?.[locationName] && data.locations?.[locationName]['residents'] && Array.isArray(data.locations?.[locationName]['residents'])) ? data['locations'][locationName]['residents'].length : "Unknown",
+      };
+    }
+
+    if (data.hasOwnProperty('episodes')) {
+      presets.dynamic.episodes = data['episodes'] ? getEpisodesInfo(getEpisodesIndexes(episodes), data['episodes']) : [];
+    }
+
+    return {
+      static: presets.static,
+      dynamic: {
+        profile: presets['dynamic']['profile'],
+        origin: presets['dynamic']['origin'],
+        location: presets['dynamic']['location'],
+        episodes: presets['dynamic']['episodes'],
       }
     }
   }
